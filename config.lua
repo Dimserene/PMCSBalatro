11fb7f1cabc54c30a176d945c1bf6a720aabd9bf
Dimserene <31881806+Dimserene@users.noreply.github.com>
2025-03-10 06:14:46 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,1 +1,1 @@
-return {["papermario_only"]=false,["things_added"]=true}
+return {["things_added"]=true,["bc_rarity"]=3,["papermario_only"]=false,["things_rarity"]=1,["bc_added"]=true,["drained_rarity"]=6,}
